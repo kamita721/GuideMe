e1e1e36adf7b73b9283c7e0dab5db79372dff77a
gnutp <76857202+gnutp@users.noreply.github.com>
2021-01-03 18:28:28 +1300

    Linux Wayland fixes
    Conditionally enable the following Wayland workarounds in the start
    script if Wayland is detected:
    
    * Set environment variable GDK_BACKEND=x11 to force GTK/GDK to use Xwayland.
      This avoids a SIGSEGV in libX11.
    
    * Set Java system property VLCJ_INITX=no to to disable VLCJ's X initialization
      routine.
      This avoids a SIGSEGV in libpthread.

--- start-linux.sh
+++ start-linux.sh
@@ -1,4 +1,16 @@
+#!/bin/sh
 # Set library path for SNAP packages (Ubuntu support)
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/snap/vlc/current/usr/lib
 
-java -Xms1024m -Xmx1024m -jar GuideMe.jar
+JAVA_OPTS="-Xms1024m -Xmx1024m"
+
+if [ "${XDG_SESSION_TYPE-}" = "wayland" ]; then
+	# GDK_BACKEND=x11 required for GUI to start on wayland
+	GDK_BACKEND="x11"
+	export GDK_BACKEND
+	# Disable X initialization in VLCJ to avoid SIGSEGV on wayland
+	JAVA_OPTS="${JAVA_OPTS} -DVLCJ_INITX=no"
+fi
+
+java ${JAVA_OPTS} \
+	-jar GuideMe.jar
